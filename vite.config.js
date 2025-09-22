942e975b2a493c78b342bf7f2e9d45963203982e
AVRAMENKOSEMEN <hendmei@mail>
2025-09-22 15:45:55 +0600

    Добавление собранного сайта с правильным base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gps-beacon-pwa/',  // ОБЯЗАТЕЛЬНО имя репозитория!
+  base: '/gps-beacon-pwa/',  // <-- ОБЯЗАТЕЛЬНО имя репозитория
   plugins: [react()],
 })
