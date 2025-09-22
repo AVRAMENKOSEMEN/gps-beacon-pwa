cc64c02606267e39a22c48c6bab825aecce96d8b
AVRAMENKOSEMEN <hendmei@mail>
2025-09-22 15:10:58 +0600

    Заливка собранного сайта для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gps-beacon-pwa/', // имя репозитория на GitHub
+  base: '/gps-beacon-pwa/',  // ОБЯЗАТЕЛЬНО имя репозитория!
   plugins: [react()],
 })
-
