f14ea6f8154eb0f176e58b6fd8682a9b9e92d93d
AVRAMENKOSEMEN <61841515+AVRAMENKOSEMEN@users.noreply.github.com>
2025-09-22 13:08:47 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/gps-beacon-pwa/', // имя репозитория GitHub
+  base: '/gps-beacon-pwa/',   // имя репозитория
   plugins: [react()],
 })
